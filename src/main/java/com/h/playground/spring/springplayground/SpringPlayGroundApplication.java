2ece5ff57982f2343ebb84695b4aaf17efd902ce
sqlimitless <lhjhoon@gmail.com>
2023-07-30 15:05:59 +0900

    feat: init

---
+++ src/main/java/com/h/playground/spring/springplayground/SpringPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package com.h.playground.spring.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlayGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/h/playground/spring/springplayground/SpringPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h.playground.spring.springplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
